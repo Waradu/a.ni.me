95df3d8c5eb0ec1acdbe6cfec3966dfac9b58832
Waradu <waradu@outlook.com>
2024-09-14 13:31:17 +0200

    removed ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  ssr: false,
   modules: ["nuxt-svgo", "@pinia/nuxt"],
   app: {
     pageTransition: { name: "page", mode: "out-in" },
