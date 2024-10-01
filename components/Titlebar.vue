08b4b403731a612632d81fe4fd5e60ed1aa58593
Waradu <waradu@outlook.com>
2024-10-01 19:40:51 +0200

    removed unused code from titlebar

--- components/Titlebar.vue
+++ components/Titlebar.vue
@@ -37,28 +37,6 @@
         </div>
       </div>
     </div>
-    <Modal header="Add anime" ref="modal" :onHide="() => { page = 0 }">
-      <div class="anime" v-if="anime">
-        <div class="image">
-          <img :src="anime.images[Object.keys(anime.images)[0]].image_url" alt="Image">
-        </div>
-        <div class="text">
-          <h2 class="name">{{ animeTitle }}</h2>
-          <p class="desc">{{ anime.synopsis?.replace("[Written by MAL Rewrite]", "").trim() }}</p>
-          <button class="add" @click="dbadd(anime)" :disabled="myanimenames.includes(animeTitle)">{{
-            myanimenames.includes(animeTitle) ? "Already in Library" : "Add"
-          }}</button>
-        </div>
-      </div>
-      <div class="buttons">
-        <button class="previous" @click="back" :disabled="page == 0">Back</button>
-        <div class="progress" v-if="animes.length > 0">
-          {{ page + 1 }}/{{ animes.length }}
-        </div>
-        <div class="progress" v-else>No results found</div>
-        <button class="next" @click="next" :disabled="page == animes.length - 1">Next</button>
-      </div>
-    </Modal>
   </header>
 </template>
 
@@ -71,10 +49,6 @@ import MinimizeIcon from "~/assets/svg/minimize.svg";
 import RestoreIcon from "~/assets/svg/restore.svg";
 import CloseIcon from "~/assets/svg/close.svg";
 import { getCurrentWindow } from "@tauri-apps/api/window";
-import type { Response as JikanResponse, Data as JikanData } from "~/types/response";
-import axios from "axios";
-import type { Modal } from "#build/components";
-import type { Anime } from "~/types/anime";
 import { WebviewWindow } from '@tauri-apps/api/webviewWindow'
 
 const openSettings = () => {
@@ -91,12 +65,9 @@ const openSettings = () => {
   webview.once('tauri://error', (e) => {
     console.error('Window error:', e)
   })
-  /* webview.destroy() */
 }
 
-const { $database, $emitter } = useNuxtApp();
-
-const modal = ref<InstanceType<typeof Modal> | null>(null)
+const { $emitter } = useNuxtApp();
 
 const titlebarStore = useTitlebarStore();
 const router = useRouter()
@@ -110,57 +81,7 @@ watch(height, (newHeight) => {
   document.documentElement.style.setProperty('--height', newHeight + 'px');
 });
 
-const animes = ref<JikanData[]>([]);
-const animeTitle = computed(() => {
-  return anime.value.title.replaceAll("\"", "")
-})
-let lastAnime = "";
-const myanimes = ref<Anime[]>([]);
-const myanimenames = computed(() => {
-  return myanimes.value.map((a) => a.name)
-})
-
 const mounted = ref(false);
-
-const page = ref(0);
-const anime = computed(() => {
-  return animes.value[page.value];
-})
-
-const back = () => {
-  page.value--;
-  page.value = page.value < 0 ? 0 : page.value
-}
-
-const next = () => {
-  page.value++;
-  page.value = page.value > animes.value.length - 1 ? animes.value.length - 1 : page.value
-}
-
-const keyboard = useKeyboard()
-
-keyboard.down("ArrowRight", next)
-keyboard.down("ArrowLeft", back)
-keyboard.down("Escape", () => {
-  if (!modal.value) return;
-
-  modal.value.hide()
-
-  if (!input.value) return;
-
-  input.value.focus()
-})
-keyboard.down("Enter", async () => {
-  if (!(animes.value.length > 0) || !animes.value[page.value] || myanimenames.value.includes(animeTitle.value) || !modal.value || !modal.value.status()) return;
-
-  if (lastAnime != animes.value[page.value].title) return;
-
-  await dbadd(animes.value[page.value])
-
-  if (!input.value) return;
-
-  input.value.focus()
-})
 
 const search = (event: Event) => {
   titlebarStore.setSearch((event.target as HTMLInputElement).value);
@@ -179,26 +100,14 @@ const navigateHome = async () => {
     await router.push('/')
   }
 }
-
 const add = async (event: KeyboardEvent) => {
   $emitter.emit('search');
-}
-
-const dbadd = async (anime: JikanData) => {
-  lastAnime = anime.title;
-  await $database.add(anime)
-  $emitter.emit('dataUpdated');
-
-  if (!modal.value) return;
-  modal.value.hide()
 }
 
 const isMaximized = ref(false);
 
 onMounted(async () => {
   mounted.value = true;
-
-  myanimes.value = await $database.animes();
 
   const currentWindow = await getCurrentWindow();
   isMaximized.value = await currentWindow.isMaximized();
@@ -322,79 +231,6 @@ header.titlebar {
           background-color: #ff888820;
         }
       }
-    }
-  }
-
-  button {
-    border: none;
-    background-color: transparent;
-    padding: 10px;
-    padding-inline: 20px;
-    color: white;
-    border-radius: 6px;
-    cursor: pointer;
-    font-weight: 700;
-
-    &[disabled] {
-      opacity: .5;
-      pointer-events: none;
-    }
-  }
-
-  .content {
-    display: flex;
-    flex-direction: column;
-    gap: 40px;
-  }
-
-  .anime {
-    display: flex;
-    gap: 20px;
-
-    .image {
-      overflow: hidden;
-      display: flex;
-      width: 200px;
-      aspect-ratio: 2 / 3;
-      overflow: hidden;
-      border-radius: 8px;
-
-      img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-      }
-    }
-
-    .text {
-      width: 400px;
-      display: flex;
-      flex-direction: column;
-      align-items: start;
-      justify-content: space-between;
-
-      p {
-        overflow: hidden;
-        display: -webkit-box;
-        -webkit-line-clamp: 8;
-        line-clamp: 8;
-        -webkit-box-orient: vertical;
-        height: max-content;
-      }
-
-      .add {
-        background-color: white;
-        color: black;
-      }
-    }
-  }
-
-  .buttons {
-    display: flex;
-    justify-content: space-between;
-
-    button {
-      border: 1px solid #ffffff20;
     }
   }
 }
