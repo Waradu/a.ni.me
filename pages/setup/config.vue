46203c15bd8c12fef4bdfd2763576e69c421275f
Waradu <waradu@outlook.com>
2024-10-07 14:47:42 +0200

    better checkboxes and recommended tag

--- pages/setup/config.vue
+++ pages/setup/config.vue
@@ -3,15 +3,21 @@
     <h1>Set your preferences!</h1>
     <div class="settings">
       <div class="setting">
-        <input type="checkbox" name="nsfw" id="nsfw" v-model="setupStore.showNSFW">
+        <label for="nsfw" class="container">
+          <input type="checkbox" name="nsfw" id="nsfw" v-model="setupStore.showNSFW">
+          <span class="checkmark red"></span>
+        </label>
         <label for="nsfw">Would you like to see NSFW content?</label>
         <Info :text="nsfwInfoText"></Info>
       </div>
       <div class="setting">
-        <input type="checkbox" name="tv" id="tv" v-model="setupStore.tvOnly">
-        <label for="tv">Show only anime shows (no movies or specials) <span
-            class="recommended">recommended</span></label>
+        <label for="tv" class="container">
+          <input type="checkbox" name="tv" id="tv" v-model="setupStore.tvOnly">
+          <span class="checkmark"></span>
+        </label>
+        <label for="tv">Show only anime shows (no movies or specials)</label>
         <Info :text="tvInfoText"></Info>
+        <div class="recommended">recommended</div>
       </div>
     </div>
     <div class="navigation">
@@ -57,6 +63,104 @@ const tvInfoText = "Turning this on will show only the main anime series with ep
       display: flex;
       gap: 10px;
       user-select: none;
+      accent-color: #385738;
+      align-items: center;
+
+      * {
+        cursor: pointer;
+      }
+
+      .container {
+        display: block;
+        position: relative;
+        cursor: pointer;
+        font-size: 22px;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        height: 25px;
+        width: 25px;
+        margin-top: 1px;
+
+        input {
+          position: absolute;
+          opacity: 0;
+          cursor: pointer;
+          height: 0;
+          width: 0;
+        }
+
+        .checkmark {
+          position: absolute;
+          top: 0;
+          left: 0;
+          height: 25px;
+          width: 25px;
+          background-color: #ffffff20;
+          border-radius: 4px;
+          transition: .2s ease-in-out;
+
+          &:after {
+            content: "";
+            position: absolute;
+            opacity: 0;
+            transition: .2s ease-in-out;
+            left: 9px;
+            top: 5px;
+            width: 4px;
+            height: 9px;
+            border: solid white;
+            border-width: 0 3px 3px 0;
+            -webkit-transform: rotate(45deg);
+            -ms-transform: rotate(45deg);
+            transform: rotate(45deg);
+          }
+        }
+
+        &:has(:checked) {
+          .checkmark {
+            background-color: #4a794a80;
+
+            &.red {
+              background-color: #a0484880;
+            }
+
+            &:after {
+              opacity: 1;
+            }
+          }
+        }
+      }
+
+      &:hover {
+        .checkmark {
+          background-color: #ffffff40;
+        }
+
+        &:has(:checked) {
+          .checkmark {
+            background-color: #4a794add;
+
+            &.red {
+              background-color: #a04848dd;
+            }
+          }
+
+        }
+      }
+
+      .recommended {
+        text-transform: uppercase;
+        font-size: 12px;
+        background-color: #ffffff10;
+        border: 1px solid #ffffff20;
+        color: #ffffffaa;
+        padding: 2px;
+        padding-inline: 10px;
+        border-radius: 20px;
+        margin-top: 1px;
+      }
     }
   }
 }
