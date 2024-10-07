5eb56970f8a23efdad8e14084f680c88dbcf6e4f
Waradu <waradu@outlook.com>
2024-10-07 13:37:07 +0200

    Added tippyjs

---
+++ components/Info.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="info" id="text" ref="infoIconRef">
+    <InfoIcon />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import tippy from 'tippy.js'
+import 'tippy.js/dist/tippy.css'
+import InfoIcon from "@fluentui/svg-icons/icons/info_32_filled.svg";
+
+const props = defineProps({
+  text: {
+    type: String,
+    required: true
+  }
+})
+
+const infoIconRef = ref<HTMLElement | null>(null)
+
+onMounted(() => {
+  if (infoIconRef.value) {
+    tippy(infoIconRef.value, {
+      content: props.text,
+    })
+  }
+})
+</script>
+
+<style lang="scss"></style>
