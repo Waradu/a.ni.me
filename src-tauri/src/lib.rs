107d5eb668f057511ce9806f835139f0dedc9c04
Waradu <waradu@outlook.com>
2024-10-07 18:53:19 +0200

    added window state and single instance

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,4 @@
+use tauri::Manager;
 use tauri_plugin_sql::{Migration, MigrationKind};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -11,12 +12,24 @@ pub fn run() {
         kind: MigrationKind::Up,
     }];
 
-    tauri::Builder::default()
-        .plugin(
-            tauri_plugin_sql::Builder::default()
-                .add_migrations("sqlite:a.ni.me.db", migrations)
-                .build(),
-        )
+    let mut builder = tauri::Builder::default();
+
+    builder = builder.plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+        let _ = app
+            .get_webview_window("main")
+            .expect("no main window")
+            .set_focus();
+    }));
+
+    builder = builder.plugin(tauri_plugin_window_state::Builder::default().build());
+
+    builder = builder.plugin(
+        tauri_plugin_sql::Builder::default()
+            .add_migrations("sqlite:a.ni.me.db", migrations)
+            .build(),
+    );
+
+    builder
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
