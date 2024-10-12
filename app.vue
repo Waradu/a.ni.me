49d4935d1a53dea288dab26dcb615c7ece6bcf2d
Waradu <waradu@outlook.com>
2024-10-12 11:45:06 +0200

    fixed updater

--- app.vue
+++ app.vue
@@ -13,30 +13,34 @@ import 'tippy.js/dist/svg-arrow.css';
 import { check } from '@tauri-apps/plugin-updater';
 import { relaunch } from '@tauri-apps/plugin-process';
 
-const update = await check();
-if (update) {
-  console.log(`found update ${update.version} from ${update.date} with notes ${update.body}`);
-  let downloaded = 0;
-  let contentLength = 0;
-
-  await update.downloadAndInstall((event) => {
-    switch (event.event) {
-      case 'Started':
-        contentLength = event.data.contentLength || -1;
-        console.log(`started downloading ${event.data.contentLength} bytes`);
-        break;
-      case 'Progress':
-        downloaded += event.data.chunkLength;
-        console.log(`downloaded ${downloaded} from ${contentLength}`);
-        break;
-      case 'Finished':
-        console.log('download finished');
-        break;
-    }
-  });
-
-  console.log('update installed');
-  await relaunch();
+try {
+  const update = await check();
+  if (update) {
+    console.log(`found update ${update.version} from ${update.date} with notes ${update.body}`);
+    let downloaded = 0;
+    let contentLength = 0;
+  
+    await update.downloadAndInstall((event) => {
+      switch (event.event) {
+        case 'Started':
+          contentLength = event.data.contentLength || -1;
+          console.log(`started downloading ${event.data.contentLength} bytes`);
+          break;
+        case 'Progress':
+          downloaded += event.data.chunkLength;
+          console.log(`downloaded ${downloaded} from ${contentLength}`);
+          break;
+        case 'Finished':
+          console.log('download finished');
+          break;
+      }
+    });
+  
+    console.log('update installed');
+    await relaunch();
+  }
+} catch (e) {
+  console.log(e);
 }
 
 tippy.setDefaultProps({
