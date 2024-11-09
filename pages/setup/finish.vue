287ea9904d15edbeb1392344c70a31b28bd0a390
Waradu <waradu@outlook.com>
2024-11-09 23:32:42 +0100

    feat: correct js-confetti import

--- pages/setup/finish.vue
+++ pages/setup/finish.vue
@@ -13,32 +13,25 @@
 
 <script lang="ts" setup>
 import DoneIcon from "@fluentui/svg-icons/icons/checkmark_32_filled.svg";
+import JSConfetti from "js-confetti";
 
 const setupStore = useSetupStore();
 const settingsStore = useSettingsStore();
 
-const { proxy } = useScriptNpm({
-  packageName: 'js-confetti',
-  file: 'dist/js-confetti.browser.js',
-  version: '0.12.0',
-  scriptOptions: {
-    //@ts-ignore
-    use: () => typeof window.JSConfetti !== 'undefined' && new window.JSConfetti()
-  },
-})
-
 onMounted(() => {
   if (setupStore.finished != true) {
-    settingsStore.showNSFW = setupStore.showNSFW
-    settingsStore.tvAndMovieOnly = setupStore.tvAndMovieOnly
-    proxy.addConfetti({
+    settingsStore.showNSFW = setupStore.showNSFW;
+    settingsStore.tvAndMovieOnly = setupStore.tvAndMovieOnly;
+    
+    const jsConfetti = new JSConfetti();
+    jsConfetti.addConfetti({
       confettiRadius: 6,
       confettiNumber: 800,
-    })
+    });
   }
-  setupStore.finished = true
-  setupStore.inSetup = false
-})
+  setupStore.finished = true;
+  setupStore.inSetup = false;
+});
 </script>
 
 <style lang="scss">
