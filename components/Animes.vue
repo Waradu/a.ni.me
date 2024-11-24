e57838eed6ef59535d875716f9136eecf6c8c46a
Waradu <waradu@outlook.com>
2024-11-24 21:25:19 +0100

    feat: fuse search

--- components/Animes.vue
+++ components/Animes.vue
@@ -8,6 +8,12 @@
 
 <script lang="ts" setup>
 import type { Anime } from "~/types/database";
+import Fuse from "fuse.js";
+
+const options = {
+  keys: ["data.title.english", "data.title.romaji", "data.description"],
+  threshold: 0.4,
+};
 
 const { $animes } = useNuxtApp();
 
@@ -17,62 +23,62 @@ const settingsStore = useSettingsStore();
 const filteredAnimes = computed(() => {
   const term = titlebarStore.search.toLowerCase();
 
-  var res = animes.value
-    .filter((a) => {
-      const title = a.data.title.english || a.data.title.romaji;
-      const description = a.data.description;
+  const fuse = new Fuse(animes.value, options);
 
-      return (
-        (title && title.toLowerCase().includes(term)) ||
-        (description && description.toLowerCase().includes(term))
-      );
-    })
-    .sort((a, b) => {
-      const sortBy = settingsStore.sortBy;
+  const searchAnimes = (term: string) => {
+    if (!term) return animes.value;
 
-      if (sortBy == "stars") {
-        if (a.stars > b.stars) {
-          return -1;
-        }
+    return fuse.search(term).map((result) => result.item);
+  };
 
-        if (a.stars < b.stars) {
-          return 1;
-        }
+  let res = searchAnimes(term.toLowerCase());
 
-        return 0;
-      }
+  res = res.sort((a, b) => {
+    const sortBy = settingsStore.sortBy;
 
-      if (sortBy == "date_added") {
-        return (
-          new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
-        );
-      }
-
-      var aItem;
-      var bItem;
-
-      if (sortBy == "name") {
-        aItem = a.data.title.english || a.data.title.romaji || "";
-        bItem = b.data.title.english || b.data.title.romaji || "";
-
-        return aItem.localeCompare(bItem);
-      } else {
-        aItem = a.data[sortBy as keyof typeof a.data];
-        bItem = b.data[sortBy as keyof typeof b.data];
-      }
-
-      if (!aItem || !bItem) return 0;
-
-      if (aItem > bItem) {
+    if (sortBy == "stars") {
+      if (a.stars > b.stars) {
         return -1;
       }
 
-      if (aItem < bItem) {
+      if (a.stars < b.stars) {
         return 1;
       }
 
       return 0;
-    });
+    }
+
+    if (sortBy == "date_added") {
+      return (
+        new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+      );
+    }
+
+    var aItem;
+    var bItem;
+
+    if (sortBy == "name") {
+      aItem = a.data.title.english || a.data.title.romaji || "";
+      bItem = b.data.title.english || b.data.title.romaji || "";
+
+      return aItem.localeCompare(bItem);
+    } else {
+      aItem = a.data[sortBy as keyof typeof a.data];
+      bItem = b.data[sortBy as keyof typeof b.data];
+    }
+
+    if (!aItem || !bItem) return 0;
+
+    if (aItem > bItem) {
+      return -1;
+    }
+
+    if (aItem < bItem) {
+      return 1;
+    }
+
+    return 0;
+  });
 
   if (settingsStore.order == "desc") {
     res = res.reverse();
