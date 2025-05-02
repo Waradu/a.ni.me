45d6468897f698ddd141e8f8911f315d8044b29e
Waradu <waradu@outlook.com>
2025-05-02 14:08:39 +0200

    feat: init tauri

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   ssr: false,
-})
+  devServer: { host: process.env.TAURI_DEV_HOST || "localhost" },
+  vite: {
+    clearScreen: false,
+    envPrefix: ["VITE_", "TAURI_"],
+    server: {
+      strictPort: true,
+    },
+  },
+});
