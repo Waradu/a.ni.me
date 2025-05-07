42d8f58a77a63f4cc09d88452d196fe607371c0f
Waradu <waradu@outlook.com>
2025-05-07 16:44:33 +0200

    feat: more info and clear targets

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,6 +5,7 @@ pub fn run() {
         .plugin(
             tauri_plugin_log::Builder::default()
                 .level(log::LevelFilter::Debug)
+                .clear_targets()
                 .target(tauri_plugin_log::Target::new(
                     tauri_plugin_log::TargetKind::Stdout,
                 ))
