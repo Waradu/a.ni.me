30661fac353f8a5d2e8191df7f469ad7565f27b3
Waradu <waradu@outlook.com>
2025-05-11 18:18:10 +0200

    feat: better error page

--- components/titlebar/Index.vue
+++ components/titlebar/Index.vue
@@ -4,15 +4,22 @@
     data-tauri-drag-region
   >
     <div class="w-52 pl-2 flex items-center" data-tauri-drag-region>
-      <h3 class="text-lg w-full font-semibold whitespace-nowrap overflow-hidden overflow-ellipsis" data-tauri-drag-region>
+      <h3
+        class="text-lg w-full font-semibold whitespace-nowrap overflow-hidden overflow-ellipsis"
+        data-tauri-drag-region
+      >
         {{ title }}
       </h3>
     </div>
-    <TitlebarNavigation />
+    <TitlebarNavigation v-if="!hideNavigation" />
     <TitlebarControls />
   </nav>
 </template>
 
 <script lang="ts" setup>
 const title = usePageScopedState("title");
+
+defineProps<{
+  hideNavigation?: boolean;
+}>();
 </script>
--- error.vue
+++ error.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="relative w-full h-full bg-neutral-800 bg-opacity-85 backdrop-blur-md flex flex-col">
-    <Titlebar />
+  <div
+    class="relative w-full h-full bg-neutral-800 bg-opacity-85 backdrop-blur-md flex flex-col"
+  >
+    <Titlebar hide-navigation />
     <main class="w-[calc(100%-4px)] h-full overflow-hidden overflow-y-scroll">
       <div
         class="min-h-screen flex flex-col items-center justify-center text-center p-12 px-4 gap-8"
@@ -8,8 +10,24 @@
         <div class="flex flex-col gap-4 items-center">
           <h1 class="text-5xl font-bold">Oops! {{ error.statusCode }}</h1>
           <p class="text-lg text-neutral-500">{{ error.message }}</p>
-          <UiButton text="Home" v-slot="props" @click="home">
-            <IconHome :class="props.class" />
+          <UiButton text="Home" @click="home">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-house-icon lucide-house size-4"
+            >
+              <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path>
+              <path
+                d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"
+              ></path>
+            </svg>
           </UiButton>
         </div>
         <div
@@ -21,7 +39,6 @@
         </div>
       </div>
     </main>
-    <Statusbar />
   </div>
 </template>
 
