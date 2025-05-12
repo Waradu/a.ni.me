f180e9206c77c6e2ef84e26a9760e04deb76e18e
Waradu <waradu@outlook.com>
2025-05-12 06:01:42 +0200

    feat: disabled button

--- components/ui/Button.vue
+++ components/ui/Button.vue
@@ -1,11 +1,12 @@
 <template>
   <button
-    class="cursor-pointer flex gap-2 items-center justify-center transition-colors h-8 px-4 border-[1px] border-neutral-700 hover:bg-neutral-700 rounded-full text-neutral-200 hover:text-white relative text-sm w-max outline-none"
+    class="cursor-pointer select-none flex gap-2 items-center justify-center transition-colors h-8 px-4 border-[1px] border-neutral-700 hover:bg-neutral-700 rounded-full text-neutral-200 hover:text-white relative text-sm w-max outline-none"
     :class="[
       loading
         ? 'animate-pulse text-white bg-neutral-700 pointer-events-none'
         : '',
       secondary ? 'bg-neutral-700 hover:bg-opacity-50' : '',
+      disabled? 'bg-neutral-700 opacity-50 pointer-events-none' : ''
     ]"
   >
     <span>{{ text }}</span>
@@ -15,5 +16,5 @@
 </template>
 
 <script lang="ts" setup>
-defineProps<{ text: string; loading?: boolean; secondary?: boolean }>();
+defineProps<{ text: string; loading?: boolean; secondary?: boolean, disabled?: boolean }>();
 </script>
